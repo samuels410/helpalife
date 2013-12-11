6f1c0bf773ac92fcf27fbc4b863743378d4fce1c
samuel santhosh <samuel@arrivusystems.com>
2013-12-11 22:51:27 +0530

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Community
       g.view_specs false
       g.helper_specs false
     end
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
