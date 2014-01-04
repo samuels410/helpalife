9a128681c62b2b653a39ab3201458775ca65a7b5
samuel santhosh <samuel@arrivusystems.com>
2014-01-04 23:03:35 +0530

    add meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    base_title = "Indian Blood Doners"
+    base_title = "Blood Donors"
     @page_title = page_title
     if page_title.empty?
       base_title
