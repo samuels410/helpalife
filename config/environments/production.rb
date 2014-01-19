176c617e25536451b75000f8d275e049c780fcc8
samuel santhosh <samuel@arrivusystems.com>
2014-01-19 10:58:08 +0530

    change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Community::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -21,7 +21,7 @@ Community::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
