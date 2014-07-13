2698268e1e6c52fe02c90572317d9b3a964cc9dd
samuel santhosh <samuel@arrivusystems.com>
2014-07-13 23:04:57 +0530

    fix assets compile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,5 +37,12 @@ class ApplicationController < ActionController::Base
     request.format = :mobile if mobile_device?
   end
 
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+     root_path
+  end
+
 end
 
