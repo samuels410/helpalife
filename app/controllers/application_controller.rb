dad3cc00a4db04fb245be50b061bd8ebb244d734
samuel santhosh <samuel@arrivusystems.com>
2014-07-22 23:23:01 +0530

    improve seo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,13 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  MOBILE_USER_AGENTS =  'Mobile|webOS|palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+      'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+      'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+      'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+      'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+      'mobile|zune'
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation,:phone_no,:blood_group,
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    base_title = "Donate blood & Blood donors"
+    base_title = "Voluntary blood donors community"
     @page_title = page_title
     if page_title.empty?
       base_title
