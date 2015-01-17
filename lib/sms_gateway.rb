d52072227463beee976dd300ccb1098c52e807f0
samuel santhosh <samuel@arrivusystems.com>
2015-01-17 22:51:53 +0530

    update email address

--- lib/sms_gateway.rb
+++ lib/sms_gateway.rb
@@ -3,7 +3,7 @@ module SmsGateway
 class Notifier < ActiveRecord::Base
 
   def self.send_sms_to_gateway(phone_nos,msg)
-    # begin
+    begin
       uri = URI.parse(Settings.sms_gateway)
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
@@ -13,10 +13,10 @@ class Notifier < ActiveRecord::Base
           'Authorization' => "Token token=#{Settings.sms_gateway_api}"
       }
       resp, data = http.post(path, data, headers)
-    # rescue
-    #   logger.error "error while sending sms"
-      # ignored
-    # end
+    rescue => e
+      logger.error "error while sending sms"
+      ignored
+    end
   end
 
   end
