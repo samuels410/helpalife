21b5d4b87e4f9f964a3cd81654c363a4c6cf6f83
Carl Tablante <carl.q.tablante@gmail.com>
2015-01-21 04:30:30 +0800

    local setup and gems added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,36 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  name                   :text
+#  blood_group            :text
+#  country_id             :integer
+#  state_id               :integer
+#  district_id            :integer
+#  email                  :text             default(""), not null
+#  encrypted_password     :text             default(""), not null
+#  avatar_url             :text
+#  phone_no               :text
+#  phone_no_visibility    :boolean          default(TRUE)
+#  reset_password_token   :text
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :text
+#  last_sign_in_ip        :text
+#  created_at             :datetime
+#  updated_at             :datetime
+#  can_send_email         :boolean          default(TRUE)
+#  can_send_sms           :boolean          default(TRUE)
+#  avatar_file_name       :string(255)
+#  avatar_content_type    :string(255)
+#  avatar_file_size       :integer
+#  avatar_updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
   rolify
   belongs_to :state
@@ -20,7 +53,7 @@ class User < ActiveRecord::Base
                        :content_type => { :content_type => ["image/jpeg","image/jpg", "image/gif", "image/png"] },
                        :size => { :in => 0..200.kilobytes }
 
-  validates :name,:blood_group,:state_id,:district_id,:email,:phone_no, presence: true
+  validates :name,:blood_group,:email,:phone_no, presence: true
   validates :terms_of_service, acceptance: { accept: '1' }
   validates :phone_no, length: { is: 10 }, numericality: true
 
