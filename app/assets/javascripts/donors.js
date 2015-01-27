d87f0306890ccd9db06bab8e306d26413742b491
samuel santhosh <samuel@arrivusystems.com>
2015-01-27 23:56:16 +0530

    merge carl refactors

--- app/assets/javascripts/donors.js
+++ app/assets/javascripts/donors.js
@@ -1,12 +1,12 @@
-
+//    for search doners
 jQuery(function() {
-    var districts;
-    districts = $('#user_district_id').html();
+    var UserDistricts;
+    UserDistricts = $('#user_district_id').html();
     return $('#user_state_id').change(function() {
         var escaped_state, options, state;
         state = $('#user_state_id :selected').text();
         escaped_state = state.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
-        options = $(districts).filter("optgroup[label='" + escaped_state + "']").html();
+        options = $(UserDistricts).filter("optgroup[label='" + escaped_state + "']").html();
         if (options) {
             $('#user_district_id').html(options);
             $('#user_district_id').show();
@@ -19,6 +19,32 @@ jQuery(function() {
     });
 });
 
+
+//for post need selection
+
+jQuery(function() {
+    var districts;
+    districts = $('#need_district_id').html();
+    return $('#need_state_id').change(function() {
+        var escaped_state, options, state;
+        state = $('#need_state_id :selected').text();
+        escaped_state = state.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = $(districts).filter("optgroup[label='" + escaped_state + "']").html();
+        if (options) {
+            $('#need_district_id').html(options);
+            $('#need_district_id').show();
+            return $('#district_label').show();
+        } else {
+            $('#need_district_id').empty();
+            $('#need_district_id').hide();
+            return $('#district_label').hide();
+        }
+    });
+});
+
+
+//bread crumps
+
 $('.btn-group a').on('click', function(event){
     event.preventDefault();
     var input = $(this).siblings('.control-group').find('input[type=hidden]');
@@ -27,4 +53,19 @@ $('.btn-group a').on('click', function(event){
             input.val($(this).data('value')).trigger('change');
         }
     }
+});
+
+//Need validation
+$("#post_need").click(function() {
+    if ($("#need_blood_group").find("option:selected" ).text() == '- Choose blood group -') {
+        alert("Please select a blood group");}
+
+    if ($("#need_state_id").find("option:selected" ).text() == '- Select state -'){
+        alert("Please select state");}
+
+    if ($("#need_district_id").find("option:selected" ).text() == '- Select district -'){
+        alert("Please select district");}
+
+
+
 });
