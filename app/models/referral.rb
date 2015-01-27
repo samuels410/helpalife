a25af41e428d3e97fc4c1edc5880f3aec8848146
carl <carl@serviceseeking.com.au>
2015-01-27 14:16:55 +0800

    removed annotation

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,11 +1,2 @@
-# == Schema Information
-#
-# Table name: referrals
-#
-#  id         :integer          not null, primary key
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 class Referral < ActiveRecord::Base
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                     :integer          not null, primary key
-#  name                   :text
-#  blood_group            :text
-#  country_id             :integer
-#  state_id               :integer
-#  district_id            :integer
-#  email                  :text             default(""), not null
-#  encrypted_password     :text             default(""), not null
-#  avatar_url             :text
-#  phone_no               :text
-#  phone_no_visibility    :boolean          default(TRUE)
-#  reset_password_token   :text
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :text
-#  last_sign_in_ip        :text
-#  created_at             :datetime
-#  updated_at             :datetime
-#  can_send_email         :boolean          default(TRUE)
-#  can_send_sms           :boolean          default(TRUE)
-#  avatar_file_name       :string(255)
-#  avatar_content_type    :string(255)
-#  avatar_file_size       :integer
-#  avatar_updated_at      :datetime
-#
-
 class User < ActiveRecord::Base
   rolify
 
