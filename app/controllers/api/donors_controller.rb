461a22dbc1ad702c62a4822e31041979d4ba2f58
samuel santhosh <samuels410@gmail.com>
2015-11-08 11:19:58 +0530

    merge donors api

--- app/controllers/api/donors_controller.rb
+++ app/controllers/api/donors_controller.rb
@@ -1,4 +1,6 @@
 class Api::DonorsController < Api::ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @donors = User.paginate page: params[:page], per_page: 50
     respond_with @donors
