a98e02f41e1d84f62242300b6184e14ec275c9fd
samuel santhosh <samuels410@gmail.com>
2018-01-14 00:08:33 +0530

    change deploment server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 # Define roles, user and IP address of deployment server
 # role :name, %{[user]@[IP adde.]}
-role :app, %w{sysadmin@163.172.211.191}
-role :web, %w{sysadmin@163.172.211.191}
-role :db,  %w{sysadmin@163.172.211.191}
+role :app, %w{sysadmin@163.172.81.180}
+role :web, %w{sysadmin@163.172.81.180}
+role :db,  %w{sysadmin@163.172.81.180}
 
 # Define server(s)
-server '163.172.211.191', user: 'sysadmin', roles: %w{web}
+server '163.172.81.180', user: 'sysadmin', roles: %w{web}
 
 # SSH Options
 # See the example commented out section in the file
