c4cb353a4d5ffa360d930e5da41f4e20e03f1f6f
anish55-web <p.t.l.anish789@gmail.com>
2025-02-19 17:16:25 +0530

    updates

---
+++ db/migrate/20250217113409_addingnewstates.rb
@@ -0,0 +1,8 @@
+class Addingnewstates < ActiveRecord::Migration
+  def change
+    State.where(name: ['Daman and Diu', 'Dadra and Nagar Haveli']).delete_all
+    State.find_or_create_by!(name: "Dadra and Nagar Haveli and Daman and Diu")
+    State.find_or_create_by!(name: "Telangana")
+    State.find_or_create_by!(name: "Ladakh")
+  end
+end
