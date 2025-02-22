59afd35951c781fed83ee262568d6ea3604ec2f6
anish55-web <p.t.l.anish789@gmail.com>
2025-02-22 16:02:42 +0530

    updatestates

--- db/migrate/20250222044325_updating_states.rb
+++ db/migrate/20250222044325_updating_states.rb
@@ -1,8 +1,8 @@
 class UpdatingStates < ActiveRecord::Migration
   def change
     State.where(name: ['Daman and Diu', 'Dadra and Nagar Haveli']).delete_all
-    State.find_or_create_by!(name: "Dadra and Nagar Haveli and Daman and Diu")
-    State.find_or_create_by!(name: "Telangana")
-    State.find_or_create_by!(name: "Ladakh")
+    State.first_or_create!(name: "Dadra and Nagar Haveli and Daman and Diu")
+    State.first_or_create!(name: "Telangana")
+    State.first_or_create!(name: "Ladakh")
   end
 end
